1eb26dc1e18925b73349170f14fd3e04e88175b9
TNXG <tnxg@outlook.jp>
2024-12-05 22:39:41 +0800

    refactor: Refactoring with Nitro.js

---
+++ src/routes/index.ts
@@ -0,0 +1,28 @@
+import { eventHandler, type H3Event } from "h3";
+
+interface ResponseData {
+	version: number;
+	ServerTime: Date;
+	UserAgent: string;
+	IP: string;
+}
+
+export default eventHandler(async (event: H3Event) => {
+	const headers = getHeaders(event);
+	const ip = headers["cf-connecting-ip"] || headers["x-forwarded-for"] || headers["remote-addr"];
+	const ua = headers["user-agent"];
+
+	const response: ApiResponse<ResponseData> = {
+		code: "200",
+		message: "这里是天翔TNXGの空间站的api接口！使用Nitro.js搭建，部分信息会从这里汇总发布！（迷子でもいい、迷子でも進め。 ",
+		status: "success",
+		data: {
+			version: 2,
+			ServerTime: new Date(),
+			UserAgent: ua || "未知",
+			IP: ip || "未知",
+		},
+	};
+
+	return response;
+});
