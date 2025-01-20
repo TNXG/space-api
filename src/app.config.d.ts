a0a1f3d7799157f9cc5a5f5693199c027072fbf9
TNXG <tnxg@outlook.jp>
2025-01-21 00:16:10 +0800

    feat: 添加 MongoDB 支持、友链信息提供以及友链分页功能，更新依赖项，移除 Biome 配置

--- src/app.config.d.ts
+++ src/app.config.d.ts
@@ -1,11 +1,19 @@
 declare global {
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 	export interface ApiResponse<Data = any> {
 		code: string;
-		message?: string;
+		message?: string | object;
 		status: string;
 		data?: Data;
 	}
 }
 
 export { };
+
+/*
+403 授权错误
+404 资源不存在
+500 服务器错误
+200 成功
+201 创建成功
+400 参数错误
+*/
