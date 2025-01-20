0f838aecb8bfaa2147db83f970eb73527601fc85
TNXG <tnxg@outlook.jp>
2025-01-21 00:16:10 +0800

    feat: 添加 MongoDB 支持、友链信息提供以及友链分页功能，更新依赖项，移除 Biome 配置

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,16 @@
-// https://nitro.unjs.io/config
+import process from "node:process";
 import { fileURLToPath } from "node:url";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const runtimeEnv = {
+	JWT_SECRET: process.env.JWT_SECRET,
+	MONGO_HOST: process.env.MONGO_HOST,
+	MONGO_PORT: process.env.MONGO_PORT,
+	MONGO_USER: process.env.MONGO_USER,
+	MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+};
 
 export default defineNitroConfig({
 	srcDir: "src",
@@ -9,5 +20,11 @@ export default defineNitroConfig({
 	},
 	alias: {
 		"@": fileURLToPath(new URL("./src", import.meta.url)),
+	},
+	runtimeConfig: {
+		...runtimeEnv,
+		public: {
+			baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://api-space.tnxg.top",
+		},
 	},
 });
